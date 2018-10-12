73f22ac51ca6a771038a898fdcce365efaecb932
Elti Shehu <shehuelti@gmail.com>
2018-10-12 10:21:11 +0200

    Init project add

---
+++ src/main/java/com/eltishehu/reactiveapp/EltiReactiveMongoRecipeAppApplication.java
@@ -0,0 +1,12 @@
+package com.eltishehu.reactiveapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EltiReactiveMongoRecipeAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EltiReactiveMongoRecipeAppApplication.class, args);
+    }
+}
