872df8bd5356ce79e188692989d024188de335f8
Elti Shehu <shehuelti@gmail.com>
2018-10-16 14:18:23 +0200

    Added a router function for /api/recipes

---
+++ src/main/java/com/eltishehu/reactiveapp/config/WebConfig.java
@@ -0,0 +1,29 @@
+package com.eltishehu.reactiveapp.config;
+
+import com.eltishehu.reactiveapp.domain.Recipe;
+import com.eltishehu.reactiveapp.services.RecipeService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
+
+/**
+ * Created by e.sh. on 16-Oct-18
+ */
+@Configuration
+public class WebConfig {
+
+    @Bean
+    RouterFunction<?> routes(RecipeService recipeService){
+        return RouterFunctions.route(GET("/api/recipes"),
+                serverRequest -> ServerResponse
+                                    .ok()
+                                    .contentType(MediaType.APPLICATION_JSON)
+                                    .body(recipeService.getRecipes(), Recipe.class));
+    }
+
+}
