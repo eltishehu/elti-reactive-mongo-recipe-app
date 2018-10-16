c47d131aadbfb31bafe5d7747f1dac68502b7bdf
Elti Shehu <shehuelti@gmail.com>
2018-10-16 13:57:24 +0200

    Added WebFlux Exception Handler to RecipeController

--- src/main/java/com/eltishehu/reactiveapp/controllers/RecipeController.java
+++ src/main/java/com/eltishehu/reactiveapp/controllers/RecipeController.java
@@ -1,13 +1,16 @@
 package com.eltishehu.reactiveapp.controllers;
 
 import com.eltishehu.reactiveapp.commands.RecipeCommand;
+import com.eltishehu.reactiveapp.exceptions.NotFoundException;
 import com.eltishehu.reactiveapp.services.RecipeService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.*;
+import org.thymeleaf.exceptions.TemplateInputException;
 
 //import org.springframework.web.servlet.ModelAndView;
 
@@ -84,19 +87,16 @@ public class RecipeController {
         return "redirect:/";
     }
 
-    /*@ResponseStatus(HttpStatus.NOT_FOUND)
-    @ExceptionHandler(NotFoundException.class)
-    public ModelAndView handleNotFound(Exception exception){
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler({NotFoundException.class, TemplateInputException.class})
+    public String handleNotFound(Exception exception, Model model){
 
         log.error("Handling not found exception");
         log.error(exception.getMessage());
 
-        ModelAndView modelAndView = new ModelAndView();
+        model.addAttribute("exception", exception);
 
-        modelAndView.setViewName("404error");
-        modelAndView.addObject("exception", exception);
-
-        return modelAndView;
-    }*/
+        return "404error";
+    }
 
 }
